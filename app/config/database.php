f873b65a61f791c6688673be3d4af7c555e0c679
unknown <anthony.guertin@gmail.com>
2014-11-04 22:33:45 -0700

    #3Fixed database naming convention.

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'Comment_Board',
+			'database'  => 'comment_board',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
