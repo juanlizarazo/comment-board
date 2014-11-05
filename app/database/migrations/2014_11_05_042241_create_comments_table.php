ecfbae57bee3502ccb848fca4eecc1493ad589ff
Juan Lizarazo <juan@artistmecca.com>
2014-11-04 21:37:46 -0700

    #2 Fix whitespaces

--- app/database/migrations/2014_11_05_042241_create_comments_table.php
+++ app/database/migrations/2014_11_05_042241_create_comments_table.php
@@ -5,30 +5,30 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateCommentsTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('comments', function(Blueprint $table)
-		{
-			$table->increments('id');
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('comments', function(Blueprint $table)
+    {
+      $table->increments('id');
       $table->integer('post_id');
       $table->string('comment');
       $table->timestamps();
-		});
-	}
+    });
+  }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('comments');
-	}
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::drop('comments');
+  }
 
 }
