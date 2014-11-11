cbeee5bd5717ab3a6614e9d5ba99494a30dd077d
anthonyguertin <anthony.guertin@gmail.com>
2014-11-10 22:16:47 -0500

    #8 Add header reference blade.wall

---
+++ app/views/partials/header.blade.php
@@ -0,0 +1,6 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title> a&j comment-board</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+  </head>
